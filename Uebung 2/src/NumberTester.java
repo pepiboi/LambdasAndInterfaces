0921b5b90a736dac40b65d3127472c2d803c36b1
Petar Ivanovic <peta.ivanovi12345@gmail.com>
2021-09-27 11:43:40 +0200

    zweite uebung erster Schritt fertig

---
+++ Uebung 2/src/NumberTester.java
@@ -0,0 +1,24 @@
+public class NumberTester {
+
+    
+
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester){
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+
+    }
+}
