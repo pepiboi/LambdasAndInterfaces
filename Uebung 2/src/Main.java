e1436c30b1bf8a7215b106881fa4da61bb948670
Petar Ivanovic <peta.ivanovi12345@gmail.com>
2021-10-10 19:35:37 +0200

    methods and start of Main finished Uebung 2

--- Uebung 2/src/Main.java
+++ Uebung 2/src/Main.java
@@ -1,7 +1,40 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Scanner s = new Scanner(System.in);
 
+        System.out.println("Willkommen zum Filetester! Geben Sie den Pfad zu Ihrer gewünschten Datei an.");
+        NumberTester nt = new NumberTester(s.nextLine());
+        while(!nt.working()){
+            System.out.println("Geben Sie einen Gültigen Pfad ein oder stellen Sie sicher, dass die Datei ist eine .txt Datei ist.");
+            nt.setFile(s.nextLine());
+        }
+        nt.setOddEvenTester( (n) -> (n % 2) == 0);
+        nt.setPalindromeTester((n) -> {
+            String str = Integer.toString(n);
+            int i = 0, j = str.length() - 1;
+            while (i < j) {
+                if (str.charAt(i) != str.charAt(j)) {
+                    return false;
+                }
+                ++i;
+                --j;
+            }
+            return true;
+        });
+        nt.setPrimeTester((n) -> {
+            if (n <= 1) return false;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        });
 
+        nt.testFile();
     }
 }
--- Uebung 2/src/NumberTester.java
+++ Uebung 2/src/NumberTester.java
@@ -1,9 +1,14 @@
+import java.io.*;
+
 public class NumberTester {
 
-    
+    NumberTest oddTester;
+    NumberTest primeTester;
+    NumberTest palindromeTester;
+    private File file;
 
     public NumberTester(String fileName){
-
+        file = new File(fileName);
     }
 
     public void setOddEvenTester(NumberTest oddTester){
@@ -18,7 +23,23 @@ public class NumberTester {
         this.palindromeTester = palindromeTester;
     }
 
-    public void testFile(){
+    public boolean working(){
+        if(file.exists()){
+            return file.canRead();
+        }else{
+            return false;
+        }
+    }
 
+    public void setFile(String fileName){
+        file = new File(fileName);
+    }
+
+    public void testFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String str = br.readLine();
+        while((str = br.readLine()) != null){
+
+        }
     }
 }
